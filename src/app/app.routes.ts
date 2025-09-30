033fb93afc5a344c68c597b4523533a1fbb99009
justinhutchens <jhutch2342@gmail.com>
2025-09-30 13:12:38 -0500

    feat - contact form added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ScholarshipsComponent } from './scholarships/scholarships.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'scholarships', component: ScholarshipsComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: 'home' },
 ];
